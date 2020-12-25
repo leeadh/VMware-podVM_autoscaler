da65f817e48b3dbad28e8c5008f2c280280f9058
root <leexhadrian@gmail.com>
2020-12-25 15:41:15 +0000

    changed

--- hpa_autoscaler.py
+++ hpa_autoscaler.py
@@ -33,7 +33,6 @@ def setup_args():
                         help='Set S if you want to connect securely')
     return cli.prompt_for_password(parser.parse_args())
 
-
 def get_obj(si, root, vim_type):
     container = si.content.viewManager.CreateContainerView(root, vim_type,
                                                            True)
@@ -41,43 +40,25 @@ def get_obj(si, root, vim_type):
     container.Destroy()
     return view
 
-
-def create_filter_spec(pc, vms, prop):
-    objSpecs = []
-    for vm in vms:
-        objSpec = vmodl.query.PropertyCollector.ObjectSpec(obj=vm)
-        objSpecs.append(objSpec)
-    filterSpec = vmodl.query.PropertyCollector.FilterSpec()
-    filterSpec.objectSet = objSpecs
-    propSet = vmodl.query.PropertyCollector.PropertySpec(all=False)
-    propSet.type = vim.VirtualMachine
-    propSet.pathSet = [prop]
-    filterSpec.propSet = [propSet]
-    return filterSpec
-
-
 def hpa_algo(vms, memory_threshold_utilization):
     arr =[]
     for vm in vms:
+    
         summary= vm.summary
         utilizationrate = summary.quickStats.guestMemoryUsage/summary.config.memorySizeMB
         if ("harbor" not in summary.config.name ):
-            d = {"pod_name":summary.config.name.rsplit('-', 2)[0],"mem_utilization":utilizationrate}
-        arr.append(d)
+            
+            d = {"pod_name":summary.config.name.rsplit('-', 2)[0],"mem_utilization":utilizationrate, "type": summary.config.guestId}
+            arr.append(d)
     df = pd.DataFrame(arr)
+    df = df.loc[df['type'] == "crxPod1Guest"]
+    print(df.sort_values('pod_name',ascending=False))
     dftemp= df.groupby('pod_name', as_index=False).sum()
     dftemp['desired_mem_utilization']=float(memory_threshold_utilization) 
     dftemp['desired_replicas'] = (dftemp['mem_utilization']/dftemp['desired_mem_utilization']).apply(np.ceil)
     dftemp['desired_replicas'] = np.round(dftemp['desired_replicas']).astype('Int64')
     d = dftemp.to_dict('records')
     return d
-
-def filter_results(result, value):
-    vms = []
-    for o in result.objects:
-        if o.propSet[0].val == value:
-            vms.append(o.obj)
-    return vms
 
 
 def main():
@@ -106,28 +87,19 @@ def main():
                             user=os.getenv('VC_USERNAME'),
                             pwd=os.getenv('VC_PASSWRD'),
                             port=os.getenv('VC_PORT'))
-
-    # Start with all the VMs from container, which is easier to write than
-    # PropertyCollector to retrieve them.
     vms = get_obj(si, si.content.rootFolder, [vim.VirtualMachine])
-
-    pc = si.content.propertyCollector
-    filter_spec = create_filter_spec(pc, vms, filter_key)
-    options = vmodl.query.PropertyCollector.RetrieveOptions()
-    result = pc.RetrievePropertiesEx([filter_spec], options)
-    vms = filter_results(result, value)
+    
     hpa_vms = hpa_algo(vms,memory_threshold_utilization)
 
     current_deployment = subprocess.check_output(["kubectl", "get","deployment","-o=jsonpath={.items[*].metadata.name}"]).decode().split()
     hpa_vms = [d for d in hpa_vms if d['pod_name'] in current_deployment]
-    print(current_deployment)
     print(hpa_vms)
-    
     for vm in hpa_vms:
-        #print(vm['pod_name'])
+        print(vm['pod_name'])
         deployment = vm['pod_name']
         replica_count = "--replicas="+str(vm['desired_replicas'])
         subprocess.call(["kubectl","scale","deployments",deployment,replica_count])
+
     
     
 
